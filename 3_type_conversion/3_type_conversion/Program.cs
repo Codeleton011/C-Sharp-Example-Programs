20d427628cfc73e14edbe950f386cd9620388a1e
Codeleton011 <ricsisuper@gmail.com>
2017-05-21 19:36:55 +0200

    3_type_conversion has been added!

---
+++ 3_type_conversion/3_type_conversion/Program.cs
@@ -0,0 +1,38 @@
+﻿// 3_type_conversion
+// Author: Codeleton011
+// Date: 2017. 05. 21
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_type_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Explicit type conversion Example
+            double PI = 3.14;
+            int num;
+
+            num = (int)PI; // Cast double to int
+            Console.WriteLine("{0}", num); 
+
+            //Implicit type conversion Example
+
+            double d = 6.28;
+
+            string str;
+
+            str = d.ToString(); // Note: d transformed into string
+
+            Console.WriteLine(str);
+
+
+            Console.ReadKey();
+        }
+    }
+}
