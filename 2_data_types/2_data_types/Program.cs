c26672b6303d0c2d1cb8533801a5f85160d6a82f
Codeleton011 <ricsisuper@gmail.com>
2017-05-21 19:28:53 +0200

    2_data_types has been added!

---
+++ 2_data_types/2_data_types/Program.cs
@@ -0,0 +1,34 @@
+﻿// 2_data_types
+// Author: Codeleton011
+// Date: 2017. 05. 21
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            float foo = 20.3F;
+            const double PI = 3.14; // Const means -> Not changeable
+            string hello = "Hello";
+            bool used = true;
+            //....
+
+            // {0} like %type in C
+            Console.WriteLine("Number: {0}", num);
+            Console.WriteLine("Float: {0}", foo);
+            Console.WriteLine("Double: {0}", PI);
+            Console.WriteLine("String: {0}", hello);
+            Console.WriteLine("Boolean: {0}", used);
+
+            Console.ReadKey();
+        }
+    }
+}
