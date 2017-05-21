86170aadde84ae036cc3b101c298d87356660d12
Codeleton011 <ricsisuper@gmail.com>
2017-05-21 19:47:12 +0200

    4_basic_user_input has been added!

---
+++ 4_basic_user_input/4_basic_user_input/Program.cs
@@ -0,0 +1,36 @@
+﻿// 4_basic_user_input
+// Author: Codeleton011
+// Date: 2017. 05. 21
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_basic_user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test;
+            int d;
+
+            // Basic user input
+            Console.WriteLine("Enter a value:");
+            test = Console.ReadLine();
+
+            Console.WriteLine("\n" + test);
+
+            // Basic user input 
+            // Using System.Convert
+
+            d = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(d);
+
+            Console.ReadKey();
+        }
+    }
+}
