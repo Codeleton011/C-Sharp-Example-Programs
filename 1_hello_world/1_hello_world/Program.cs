c671192d20acdfe78ad73ac0ccba90adf75c27e0
Codeleton011 <ricsisuper@gmail.com>
2017-05-21 19:16:30 +0200

    1 hello_world added!

---
+++ 1_hello_world/1_hello_world/Program.cs
@@ -0,0 +1,23 @@
+﻿// 1_hello_world
+// Author: Codeleton011
+// Date: 2017. 05. 21
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! \n"); // \n new line
+            Console.WriteLine("Hello C#!");
+
+            Console.ReadKey();
+        }
+    }
+}
