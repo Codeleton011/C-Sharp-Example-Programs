80f091c3d7b16f3223a231d758f84930f3969fc9
Codeleton011 <ricsisuper@gmail.com>
2017-05-22 17:37:25 +0200

    9_Array has been added!

---
+++ 9_Array/9_Array/Program.cs
@@ -0,0 +1,29 @@
+﻿// 9_Array
+// Author: Codeleton011
+// Date: 2017. 05. 22
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define a new array
+            int[] values = new int[5] { 1, 2, 3, 4, 5 };
+
+            // Accessing the arrays
+            for(int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine("values[{0}] = {1}", i, values[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
