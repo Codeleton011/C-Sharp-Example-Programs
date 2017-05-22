2e0cb589b3a9665d2298746fe5224e680c7e7c1e
Codeleton011 <ricsisuper@gmail.com>
2017-05-22 17:06:37 +0200

    6_Encapsulation has been added!

---
+++ 6_Encapsulation/6_Encapsulation/Program.cs
@@ -0,0 +1,43 @@
+﻿// 6_encapsulation
+// Author: Codeleton011
+// Date: 2017. 05. 22
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// C# Encapsulation with public accessor
+namespace _6_Encapsulation
+{
+    class Employee
+    {
+        // Public access!
+        public int age;
+        public string name;
+
+        public void displayEmployee()
+        {
+            Console.WriteLine("Employee Name: {0}", name);
+            Console.WriteLine("Employee Age: {0}", age);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare employee
+            Employee emp1 = new Employee();
+
+            // Set values
+            emp1.age = 20;
+            emp1.name = "Employee1";
+
+            emp1.displayEmployee();
+
+            Console.ReadKey();
+        }
+    }
+}
