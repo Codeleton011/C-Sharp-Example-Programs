9dc2786a69b22ae4d8977672c6659454f37e2a31
Codeleton011 <ricsisuper@gmail.com>
2017-05-23 21:07:11 +0200

    Added!

---
+++ 16_Interfaces/16_Interfaces/Program.cs
@@ -0,0 +1,56 @@
+﻿// 16_Interfaces
+// Author: Codeleton011
+// Date: 2017. 05. 23
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Interfaces
+{
+    public interface Shape
+    {
+        void display();
+        int getWide();
+    }
+    
+    // Interfaces can be inherited with class
+    public class A : Shape
+    {
+        private int a;
+        private int b;
+
+        // Constructor of the A class
+        public A(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("A: {0} \nB: {0}", a, b);
+        }
+
+        public int getWide()
+        {
+            return a * b;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A shape = new A(10,10);
+
+            shape.display();
+            Console.WriteLine("Shape Wide: {0}", shape.getWide());
+
+            Console.ReadKey();
+        }
+    }
+}
