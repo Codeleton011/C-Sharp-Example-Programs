42b4cda6b83427a6110494fcbfaffdd86809a55c
Codeleton011 <ricsisuper@gmail.com>
2017-05-23 20:50:08 +0200

    Added!

---
+++ 13_static_polymorphism/13_static_polymorphism/Program.cs
@@ -0,0 +1,34 @@
+﻿// 13_static_polymorphism
+// Author: Codeleton011
+// Date: 2017. 05. 23
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_static_polymorphism
+{
+    class Program
+    {
+        void print(int i)
+        {
+            Console.WriteLine("Int: {0}\n", i);
+        }
+        // Overload function
+        void print(string i)
+        {
+            Console.WriteLine("String: {0}", i);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            p.print(5);
+            p.print("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 14_dynamic_polymorphism/14_dynamic_polymorphism/Program.cs
@@ -0,0 +1,46 @@
+﻿// 14_dynamic_polymorphism
+// Author: Codeleton011
+// Date: 2017. 05. 23
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_dynamic_polymorphism
+{
+    abstract class Shape
+    {
+        public abstract int area();
+    }
+
+    class Rectangle : Shape
+    {
+        private int length;
+        private int width;
+
+        public Rectangle(int a = 0, int b = 0)
+        {
+            length = a;
+            width = b;
+        }
+        public override int area()
+        {
+            return (width * length);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10, 10);
+            double a = rect.area();
+            Console.WriteLine("Area: {0}", a);
+
+            Console.ReadKey();
+        }
+    }
+}
